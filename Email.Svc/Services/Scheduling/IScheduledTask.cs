2008e91048c8a23d776b606f81d45e165572dc25
Dmytro Stepanov <dstepanov@amtoss.com.ua>
2017-11-17 21:09:02 +0200

    Create empty .NET Core web service project
    - add NLog
    - add IScheduledTask interface
    - add resharper settings
    - add docker configs

---
+++ Email.Svc/Services/Scheduling/IScheduledTask.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Email.Svc.Services.Scheduling {
+
+    interface IScheduledTask {
+        string Schedule { get; }
+        Task ExecuteAsync(CancellationToken cancellationToken);
+    }
+
+}
